454a5bf02913f12a1920553d1a6214c5728d7190
Alberto-iesfm <alberto.benitomolina@iesfm.onmicrosoft.com>
2021-02-12 14:44:04 +0100

    Update BD_Tarea13_Alberto_Benito_Molina.sql
    tarea terminada

--- 1DAM/bases_de_datos/BD_Tarea13_Alberto_Benito_Molina.sql
+++ 1DAM/bases_de_datos/BD_Tarea13_Alberto_Benito_Molina.sql
@@ -4,6 +4,8 @@ CREATE DATABASE game_management;
 
 /* creacion de las tablas */
 
+USE game_management;
+
 CREATE TABLE genres (
 	id INT AUTO_INCREMENT NOT NULL,
     genre_name VARCHAR(20) NOT NULL,
@@ -44,3 +46,53 @@ CREATE TABLE games_publishers (
 		ON UPDATE CASCADE
         ON DELETE CASCADE
 );
+
+CREATE TABLE platforms (
+	id INT AUTO_INCREMENT NOT NULL,
+    platform_name VARCHAR(20),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE games_platforms (
+	id INT AUTO_INCREMENT NOT NULL,
+    game_publisher_id INT NOT NULL,
+    platform_id INT NOT NULL,
+    release_year INT NOT NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT FK_id_ga_pub_ga_plat
+    FOREIGN KEY (game_publisher_id)
+    REFERENCES games_publishers(id)
+		ON UPDATE CASCADE
+        ON DELETE CASCADE,
+	CONSTRAINT FK_id_plat_ga_plat
+    FOREIGN KEY (platform_id)
+    REFERENCES platforms(id)
+		ON UPDATE CASCADE
+        ON DELETE CASCADE
+);
+
+CREATE TABLE regions (
+	id INT NOT NULL,
+    region_name VARCHAR(40) NOT NULL
+);
+
+CREATE TABLE regions_sales (
+	region_id INT NOT NULL,
+    game_platform_id INT NOT NULL,
+    num_sales INT NOT NULL
+);
+
+/* añadir PK y FK a las tablas regions_sales y regions */
+
+ALTER TABLE regions
+MODIFY id INT AUTO_INCREMENT NOT NULL,
+ADD CONSTRAINT PK_id_reg
+PRIMARY KEY (id);
+
+ALTER TABLE regions_sales
+ADD CONSTRAINT PK_reg_id_ga_plat_id_reg_sal
+PRIMARY KEY (region_id, game_platform_id),
+ADD CONSTRAINT FK_id_reg_reg_sal
+FOREIGN KEY (region_id) REFERENCES regions(id),
+ADD CONSTRAINT FK_id_ga_plat_reg_sal
+FOREIGN KEY (game_platform_id) REFERENCES games_platforms(id);
