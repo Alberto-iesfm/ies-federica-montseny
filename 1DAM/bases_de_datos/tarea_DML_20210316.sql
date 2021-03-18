eeeecb9d8a4f0e6bb55406c51f5fc7d9ef5788ba
alberto-iesfm <alberto.benitomolina@iesfm.onmicrosoft.com>
2021-03-18 10:23:21 +0100

    actualizacion tarea sql

--- 1DAM/bases_de_datos/tarea_DML_20210316.sql
+++ 1DAM/bases_de_datos/tarea_DML_20210316.sql
@@ -22,7 +22,7 @@ CREATE TABLE IF NOT EXISTS empleados (
 	direccion VARCHAR(100) NOT NULL,
 	fecha_alta DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	salario	FLOAT NOT NULL CHECK (18000 <= salario <= 50000),
-	comision FLOAT NOT NULL DEFAULT 5,
+	comision FLOAT NOT NULL DEFAULT 0,
 	departamento VARCHAR(50) NOT NULL,
     CONSTRAINT FK_of_emp
     FOREIGN KEY (oficio)
@@ -49,16 +49,16 @@ VALUES 	("Ventas"),
 		("Marketing"),
         ("Informatica");
         
-INSERT INTO empleados (nombre, apellido, oficio, direccion, fecha_alta, salario, departamento)
-VALUES	("Juan", "Perez", "Comercial", "Av. de las naciones, 23", "2020-07-25", 25000, "Ventas"),
-		("Sandra", "Ruiz", "Practicas", "Plaza de la reconquista, 1", "2021-01-01", 18000, "Marketing"),
-        ("Ruben", "Alamo", "Programador", "Calle rio Duero, 23", "2005-09-03", 36000, "Informatica"),
-        ("Julian", "Garcia", "Practicas", "Av. Europa, 35", "2021-03-01", 18000, "Informatica"),
-        ("Alberto", "Hernan", "Director", "Calle de la mina, 51", "2003-05-15", 45000,"Informatica");
+INSERT INTO empleados (nombre, apellido, oficio, direccion, fecha_alta, salario, comision, departamento)
+VALUES	("Juan", "Perez", "Comercial", "Av. de las naciones, 23", "2020-07-25", 25000, salario * 0.05, "Ventas"),
+		("Sandra", "Ruiz", "Practicas", "Plaza de la reconquista, 1", "2021-01-01", 18000, salario * 0.05, "Marketing"),
+        ("Ruben", "Alamo", "Programador", "Calle rio Duero, 23", "2005-09-03", 36000, salario * 0.05, "Informatica"),
+        ("Julian", "Garcia", "Practicas", "Av. Europa, 35", "2021-03-01", 18000, salario * 0.05, "Informatica"),
+        ("Alberto", "Hernan", "Director", "Calle de la mina, 51", "2003-05-15", 45000, salario * 0.05, "Informatica");
         
 INSERT INTO empleados (nombre, apellido, oficio, direccion, fecha_alta, salario, comision, departamento)
-VALUES	("Julian", "Romeral", "Comercial", "Calle laguna, 13", "2015-04-02", 30000, 15, "Marketing"),
-		("Luis", "Alonso", "Director", "Plaza de la villa, 3", "2001-03-21", 30000, 15, "Ventas");
+VALUES	("Julian", "Romeral", "Comercial", "Calle laguna, 13", "2015-04-02", 30000, salario * 0.15, "Marketing"),
+		("Luis", "Alonso", "Director", "Plaza de la villa, 3", "2001-03-21", 30000, salario * 0.15, "Ventas");
 
 /* Actualizacion de apellidos */
 
@@ -106,4 +106,133 @@ UPDATE empleados
 SET oficio = "Director", departamento = "Ventas"
 WHERE nombre = "Sandra" AND apellido = "Ruiz";
 
-/* "ejercicio 10" */
+/* Modificacion de la comision de todos los empleados. */
+
+SELECT * FROM empleados;
+
+UPDATE empleados
+SET comision = salario * 0.10;
+
+/* Modificacion del salario de empleados que cobren mas de 32000€ */
+
+SELECT * FROM empleados
+WHERE salario < 32000 AND salario > 25000;
+
+UPDATE empleados
+SET salario = 32500
+WHERE salario < 32000 AND salario > 25000;
+
+/* Modificacion de la comision de empleados que cobren mas de 32000€ */
+
+SELECT * FROM empleados
+WHERE salario > 32000;
+
+UPDATE empleados
+SET comision = salario * 0.12
+WHERE salario > 32000;
+
+/* Incremento de salarios */
+
+SELECT * FROM empleados
+WHERE oficio = "Practicas" AND departamento = "Informatica";
+
+UPDATE empleados
+SET salario = salario + (salario * 0.05)
+WHERE oficio = "Practicas" AND departamento = "Informatica";
+
+SELECT * FROM empleados
+WHERE oficio = "Programador" AND departamento = "Informatica";
+
+UPDATE empleados
+SET salario = salario + (salario * 0.08)
+WHERE oficio = "Programador" AND departamento = "Informatica";
+
+SELECT * FROM empleados
+WHERE salario < 20000 AND departamento = "Ventas";
+
+UPDATE empleados
+SET salario = salario + (salario * 0.06)
+WHERE salario < 20000 AND departamento = "Ventas";
+
+SELECT * FROM empleados
+WHERE departamento = "Informatica";
+
+UPDATE empleados
+SET salario = salario + (salario * 0.08)
+WHERE departamento = "Informatica";
+
+/* Reduccion de salarios */
+
+SELECT * FROM empleados
+WHERE departamento = "Marketing";
+
+UPDATE empleados
+SET salario = salario - (salario * 0.03)
+WHERE departamento = "Marketing";
+
+SELECT * FROM empleados
+WHERE salario > 40000 AND oficio = "Director";
+
+UPDATE empleados
+SET salario = salario - (salario * 0.05)
+WHERE salario > 40000 AND oficio = "Director";
+
+/* Aumento de salarios 2. */
+
+SELECT * FROM empleados
+WHERE departamento = "Marketing";
+
+UPDATE empleados
+SET salario = salario + 1000
+WHERE departamento = "Marketing";
+
+SELECT * FROM empleados
+WHERE oficio = "Comercial";
+
+UPDATE empleados
+SET salario = salario + 1500
+WHERE oficio = "Comercial";
+
+SELECT * FROM empleados
+WHERE salario > 20000;
+
+UPDATE empleados
+SET salario = salario + (salario * 0.0575)
+WHERE salario > 20000;
+
+SELECT * FROM empleados
+WHERE salario < 20000;
+
+UPDATE empleados
+SET salario = salario + (salario * 0.125)
+WHERE salario < 20000;
+
+SELECT * FROM empleados
+WHERE departamento != "Ventas";
+
+UPDATE empleados
+SET salario = salario + (salario * 0.05)
+WHERE departamento != "Ventas";
+
+SELECT * FROM empleados
+WHERE departamento = "Marketing" OR departamento = "Informatica";
+
+UPDATE empleados
+SET salario = salario + (salario * 0.025)
+WHERE departamento = "Marketing" OR departamento = "Informatica";
+
+/* Actualizacion de salarios y comisiones por fecha de alta */
+
+SELECT * FROM empleados
+WHERE fecha_alta >= "2021-01-01";
+
+UPDATE empleados
+SET salario = salario + 1000, comision = salario * 0.16
+WHERE fecha_alta >= "2021-01-01";
+
+SELECT * FROM empleados
+WHERE fecha_alta < "2011-03-18";
+
+UPDATE empleados
+SET salario = salario + (salario * 0.05), comision = salario * 0.10
+WHERE fecha_alta < "2011-03-18";
