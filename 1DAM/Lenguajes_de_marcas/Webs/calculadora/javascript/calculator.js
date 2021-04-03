a4d62952747a457caeef5185eef80fe485535bee
alberto <alberto.benitomolina@iesfm.onmicrosoft.com>
2021-04-03 19:51:26 +0200

    tarea_javaScript

--- 1DAM/Lenguajes_de_marcas/Webs/calculadora/javascript/calculator.js
+++ 1DAM/Lenguajes_de_marcas/Webs/calculadora/javascript/calculator.js
@@ -1,18 +1,68 @@
+function numberNotFound(x, y){
+	return (x == "" || y == "");
+}
+
+function divisorIsZero(y){
+	return y == 0;
+}
 
 function sum() {
-	var num1 = parseInt(document.getElementById("num1").value);
-	var num2 = parseInt(document.getElementById("num2").value);
-	document.getElementById("result").innerHTML = num1 + num2;
+	var x = document.getElementById("x").value;
+	var y = document.getElementById("y").value;
+	if (numberNotFound(x, y)) {
+		document.getElementById("result").innerHTML = "";
+		window.alert("Es necesario introducir los dos numeros!");
+	} else {
+		x = parseFloat(x);
+		y = parseFloat(y);
+		document.getElementById("result").innerHTML = "El resultado de " + x + " + " + y + " es: " + (x + y);
+		document.getElementById("data").reset();
+	}
 }
 
-function subtract(num1, num2) {
-	return num1 - num2;
+function subtract() {
+	var x = document.getElementById("x").value;
+	var y = document.getElementById("y").value;
+	if (numberNotFound(x, y)) {
+		document.getElementById("result").innerHTML = "";
+		window.alert("Es necesario introducir los dos numeros!");
+	} else {
+		x = parseFloat(x);
+		y = parseFloat(y);
+		document.getElementById("result").innerHTML = "El resultado de " + x + " - " + y + " es: " + (x - y);
+		document.getElementById("data").reset();
+	}
 }
 
-function multuply(num1, num2) {
-	return num1 * num2;
+function multiply() {
+	var x = document.getElementById("x").value;
+	var y = document.getElementById("y").value;
+	if (numberNotFound(x, y)) {
+		document.getElementById("result").innerHTML = "";
+		window.alert("Es necesario introducir los dos numeros!");
+	} else {
+		x = parseFloat(x);
+		y = parseFloat(y);
+		document.getElementById("result").innerHTML = "El resultado de " + x + " * " + y + " es: " + (x * y);
+		document.getElementById("data").reset();
+	}
 }
 
-function divide(num1, num2) {
-	return num1 / num2;
+function divide() {
+	var x = document.getElementById("x").value;
+	var y = document.getElementById("y").value;
+	if (numberNotFound(x, y)) {
+		document.getElementById("result").innerHTML = "";
+		window.alert("Es necesario introducir los dos numeros!");
+	} else {
+		x = parseFloat(x);
+		y = parseFloat(y);
+		if (divisorIsZero(y)) {
+			document.getElementById("result").innerHTML = "";
+			window.alert("El divisor no puede ser 0!")
+		} else {
+			document.getElementById("result").innerHTML = "El resultado de " + x + " / " + y + " es: " + (x / y);
+			document.getElementById("data").reset();
+		}
+	}
 }
---
+++ 1DAM/programacion/socialNetwork/src/main/java/org/iesfm/socialNetwork/SocialNetwork.java
@@ -0,0 +1,4 @@
+package org.iesfm.socialNetwork;
+
+public class SocialNetwork {
+}
