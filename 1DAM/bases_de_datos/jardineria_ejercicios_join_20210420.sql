27c24ea4d65f8e13d6051f6b7289e0e346c7c22e
Alberto-iesfm <alberto.benitomolina@iesfm.onmicrosoft.com>
2021-04-21 20:08:49 +0200

    Update jardineria_ejercicios_join_20210420.sql

--- 1DAM/bases_de_datos/jardineria_ejercicios_join_20210420.sql
+++ 1DAM/bases_de_datos/jardineria_ejercicios_join_20210420.sql
@@ -65,24 +65,47 @@ WHERE ofi.Ciudad = 'Barcelona';
 -- 10. Seleccionar de todos los pedidos, el Nombre del empleado que ha realizado el pedido, 
 -- el nombre del cliente al que se le ha vendido y el código de pedido
 
+SELECT ped.CodigoPedido, cli.NombreCliente, CONCAT(emp.Nombre, ' ', emp.Apellido1, ' ', emp.Apellido2) AS 'Nombre Empleado'
+FROM pedidos ped
+LEFT JOIN clientes cli ON ped.CodigoCliente = cli.CodigoCliente
+INNER JOIN empleados emp ON cli.CodigoEmpleadoRepVentas = emp.CodigoEmpleado;
 
 -- 11. Devuelve el producto cartesiano de las tablas empleados y oficinas.
 
+SELECT * 
+FROM empleados emp
+CROSS JOIN oficinas ofi;
 
 -- 12. Mostrar el listado de pedidos que no tengan fecha de entrega, de los clientes con códigos del 1 al 15
 -- junto con los nombres de los clientes
 
+SELECT ped.*, cli.NombreCliente
+FROM pedidos ped
+INNER JOIN clientes cli ON ped.CodigoCliente = cli.CodigoCliente
+WHERE cli.CodigoCliente BETWEEN 1 AND 15 AND ped.FechaEntrega IS NULL;
+
 -- 13. Devuelve un listado de los productos que han aparecido en los pedidos, cuyo codigo de producto empiece por OR.
 
+SELECT pro.CodigoProducto, pro.Nombre
+FROM productos pro
+INNER JOIN detallepedidos deta ON pro.CodigoProducto = deta.CodigoProducto
+INNER JOIN pedidos ped ON deta.CodigoPedido = ped.CodigoPedido
+WHERE pro.CodigoProducto LIKE 'OR%'
+GROUP BY pro.Nombre;
 
 -- 14. Devuelve un listado de los productos que no han aparecido en ningún pedidos
 
+SELECT pro.CodigoProducto, pro.Nombre
+FROM productos pro
+LEFT JOIN detallepedidos deta ON pro.CodigoProducto = deta.CodigoProducto
+WHERE deta.CodigoPedido IS NULL;
 
 
 -- 15. Devuelve el código, nombre, y la descripción del texto de la gama de los productos que no han aparecido 
 -- en ningún pedido
 
-
-
-
-
+SELECT pro.CodigoProducto, pro.Nombre, CONVERT(gama.DescripcionTexto USING utf8) AS 'Descripción'
+FROM productos pro
+LEFT JOIN detallepedidos deta ON pro.CodigoProducto = deta.CodigoProducto
+INNER JOIN gamasproductos gama ON pro.Gama = gama.Gama
+WHERE deta.CodigoPedido IS NULL;
