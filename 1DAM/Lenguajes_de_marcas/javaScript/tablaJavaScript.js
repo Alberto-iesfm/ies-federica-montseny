c5cbcb806936466c1f71a58abfcdcdbd9e728aec
alberto benito <alberto.benitomolina@iesfm.onmicrosoft.com>
2021-05-07 11:00:05 +0200

    contactosJSON

--- 1DAM/Lenguajes_de_marcas/javaScript/tablaJavaScript.js
+++ 1DAM/Lenguajes_de_marcas/javaScript/tablaJavaScript.js
@@ -1,12 +1,17 @@
-function add(){
+function createUser(){
+	var newUser = {name:document.getElementById("Name").value, phone:document.getElementById("Number").value};
+	add(newUser);
+}
+
+function add(user){
 	var row = document.createElement("tr");
 
 	var column1 = document.createElement ("td");
-	var name = document.createTextNode(document.getElementById("Name").value);
+	var name = document.createTextNode(user.name);
 	column1.appendChild(name);
 
 	var column2 = document.createElement ("td");
-	var number = document.createTextNode(document.getElementById("Number").value);
+	var number = document.createTextNode(user.phone);
 	column2.appendChild(number);
 
 	row.appendChild(column1);
